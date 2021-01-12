ca1ce86772bb0f2bce80492af9cd95fc488bb8ab
Alex <alexanderesolis@gmail.com>
2021-01-12 17:05:39 -0500

    Fixed Paths for build

---
+++ Assets/Scripts/PanelController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelController : MonoBehaviour
+{
+    public CanvasGroup panel;
+    bool on = false;
+
+    public void Toggle()
+    {
+        on = !on;
+
+        if (on) panel.alpha = 1;
+        else panel.alpha = 0;
+    }
+}
