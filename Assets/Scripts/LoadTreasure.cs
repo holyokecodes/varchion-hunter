b07bba23727a9f40e5941b7baa2a670292568007
Alex <alexanderesolis@gmail.com>
2021-02-20 13:30:49 -0500

    You can actually pick them up now!

--- Assets/Scripts/LoadTreasure.cs
+++ Assets/Scripts/LoadTreasure.cs
@@ -10,6 +10,13 @@ public class LoadTreasure : MonoBehaviour
     public GameObject potteryShard;
     public GameObject electroplatedSilver;
 
+    public GameObject treasureObj;
+
+    public CanvasGroup canvasGroup;
+
+    public float time;
+    public bool hasBeenPickedUp = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,19 +24,46 @@ public class LoadTreasure : MonoBehaviour
 
         if ( treasure == "Sword")
         {
-            Instantiate(sword, new Vector3(0, 0, 2), Quaternion.identity);
+            treasureObj = Instantiate(sword, new Vector3(0, 0, 2), Quaternion.identity);
         } else if (treasure == "Knife")
         {
-            Instantiate(knife, new Vector3(0, 0, 2), Quaternion.identity);
+            treasureObj = Instantiate(knife, new Vector3(0, 0, 2), Quaternion.identity);
         } else if (treasure == "Bust")
         {
-            Instantiate(bust, new Vector3(0, 0, 2), Quaternion.identity);
+            treasureObj = Instantiate(bust, new Vector3(0, 0, 2), Quaternion.identity);
         } else if (treasure == "Pottery Shard")
         {
-            Instantiate(potteryShard, new Vector3(0, 0, 2), Quaternion.identity);
+            treasureObj = Instantiate(potteryShard, new Vector3(0, 0, 2), Quaternion.identity);
         } else if (treasure == "Weird peice of electroplated silver")
         {
-            Instantiate(electroplatedSilver, new Vector3(0, 0, 2), Quaternion.identity);
+            treasureObj = Instantiate(electroplatedSilver, new Vector3(0, 0, 2), Quaternion.identity);
         }
+    }
+
+    void Update()
+    {
+        if (hasBeenPickedUp){time += Time.deltaTime;}
+        if (time >= 2){StartCoroutine(FadeLoadingScreen(0, 1));}
+    }
+
+    public void CollectTreasure()
+    {
+        Destroy(treasureObj);
+        StartCoroutine(FadeLoadingScreen(1, 1));
+        hasBeenPickedUp = true;
+    }
+
+    IEnumerator FadeLoadingScreen(float targetValue, float duration)
+    {
+        float startValue = canvasGroup.alpha;
+        float time = 0;
+
+        while (time < duration)
+        {
+            canvasGroup.alpha = Mathf.Lerp(startValue, targetValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        canvasGroup.alpha = targetValue;
     }
 }
