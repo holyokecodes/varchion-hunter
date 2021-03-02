7f6d08cf92e3ad09a35734b5e4f7045934ab3226
Alex <alexanderesolis@gmail.com>
2021-03-02 09:23:16 -0500

    Working on automatic UI sizes

---
+++ Assets/Scripts/RelativeUISize.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RelativeUISize : MonoBehaviour
+{
+    public RectTransform canvas;
+
+    public RectTransform[] rects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < rects.Length; i++)
+        {
+            rects[i].sizeDelta = new Vector2(canvas.sizeDelta.x / rects.Length, rects[i].sizeDelta.y);
+            rects[i].position = new Vector2(canvas.sizeDelta.x / (rects.Length * 2), rects[i].anchoredPosition.y);
+        }
+    }
+}
