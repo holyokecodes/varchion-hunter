8054fe2075a285fde6e987dd5ad1fec0ea8c34b5
Alex <alexanderesolis@gmail.com>
2021-03-22 09:50:24 -0400

    Fixed the object not spawning bug

--- Assets/Scripts/LoadTreasure.cs
+++ Assets/Scripts/LoadTreasure.cs
@@ -27,26 +27,28 @@ public class LoadTreasure : MonoBehaviour
     int itemNumber = 0;
 
     public TreasureList treasures;
+    public Text treasureName;
+    public string treasureNameStr;
 
     // Start is called before the first frame update
     void Start()
     {
         //string treasure = PlayerPrefs.GetString("treasure");
         int treasure = PlayerPrefs.GetInt("treasure");
-        print("Treasure ID: " + treasure);
+        Debug.Log("Treasure ID: " + treasure);
 
         arPlaneManager.planesChanged += PlanesChanged;
 
         //string treasure = PlayerPrefs.GetString("treasure");
         
-        // REALLY IMPORTANT NOTE! if you want different values of XP for different treasures, set them here with the code "xpGain = [new value]"
-
         for (int i = 0; i < treasures.treasures.Length; i++)
         {
             if (treasures.treasures[i].ID == treasure)
             {
-                treasureObj = treasures.treasures[i].prefab;
-                print(treasures.treasures[i].displayName);
+                treasurePrefab = treasures.treasures[i].prefab;
+                Debug.Log(treasures.treasures[i].displayName);
+                treasureNameStr = treasures.treasures[i].displayName;
+                treasureName.text = treasureNameStr;
             }
         }
         //Instantiate(treasureObj, Vector3.zero, Quaternion.identity);
@@ -66,7 +68,9 @@ public class LoadTreasure : MonoBehaviour
             if (args.added.Count > 0)
             {
                 ARPlane arPlane = args.added[0];
-                treasureObj = Instantiate(treasurePrefab, arPlane.transform);//.position, Quaternion.identity);
+                treasureObj = Instantiate(treasurePrefab, arPlane.transform.position, Quaternion.identity);
+                treasureName.text = treasureNameStr;
+                Debug.Log("Made it: " + treasureNameStr);
             }
         }
     }
